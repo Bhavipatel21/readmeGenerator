83c77e11c0153d7a8b4a91b89006065af9cb418f
Bhavika Patel <bhavij28@gmail.com>
2021-02-27 18:12:36 -0500

    added questions, licenses and file creation logic

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateFile = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  //add validation for questions which can not be left blank
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project name?",
+   
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project:",
+
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email:",
+       },
+    //following questions are optional, can be left blank
+    {
+        type: "list",
+        name: "license",
+        message: "Please select license for this application :",
+        choices: ['MIT', 'IBM', 'Apache', 'Eclipse', 'None'],
+
+    },
+    {
+
+        type: "input",
+        name: "dependencies",
+        message: "What command should be ran to install dependencies?",
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be ran to install dependencies?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter instructions on how to install your application :",
+    },
+    {
+        type: "input",
+        name: "gitusername",
+        message: "Please enter your github username:",
+    }
+]
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(
+        [...questions]
+    )
+        .then((answers) => {
+            try {
+                const readme = generateFile(answers);
+                fs.writeFileSync('myREADME.md', readme);
+                console.log('README.md has been created!!')
+            }
+            catch (err) {
+                console.log(err);
+            }
+        })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,88 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(answers) {
+  let licenseBadge;
+  switch(answers.license){
+    case 'MIT':
+    licenseBadge = "![badge](https://img.shields.io/badge/license-MIT-blueviolet)";
+    break;
+    case 'IBM':
+    licenseBadge = "![badge](https://img.shields.io/badge/license-IBM-blueviolet)";
+    break;
+    case 'Apache':
+    licenseBadge = "![badge](https://img.shields.io/badge/license-Apache-blueviolet)";
+    break;
+    case 'Eclipse':
+    licenseBadge = "![badge](https://img.shields.io/badge/license-Eclipse-blueviolet)";
+    break;
+    case 'None':
+    licenseBadge = '';
+      }
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(answers) {
+  let licenseLink;
+
+  switch(answers.license) {
+    case 'MIT': 
+    licenseLink = "[MIT](https://spdx.org/licenses/MIT.html)";
+    break;
+    case 'IBM':
+    licenseLink="[IBM](https://spdx.org/licenses/IPL-1.0.html)";
+    break;
+    case 'Apache License':
+    licenseLink = "[Apache](https://spdx.org/licenses/Apache-2.0.html)";
+    break;
+    case 'Eclipse':
+    licenseLink = "[Eclipse](https://spdx.org/licenses/Apache-2.0.html)";
+    break;
+    case 'None':
+    licenseLink="";
+  }
+
+ return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(answers) {
+
+  if (answers.license === 'None') {
+    return ``;
+    
+  }else {
+    return `## License  ${renderLicenseBadge(answers)}
+  Project license : click ${renderLicenseLink(answers)}`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
+  ${renderLicenseBadge(answers)}
+  ## Description 
+   ${answers.description}
+
+  ## Installation
+   ${answers.installation}
+  ## Usage 
+   ${answers.usage}
+  ## Contributing
+   ${answers.writecontribution}
+   
+  ## Tests
+  ${answers.test}
+  ${renderLicenseSection(answers)}
+
+  ## Questions
+
+  For any questions email me at: ${answers.email}
 
 `;
 }
 
 module.exports = generateMarkdown;
+
